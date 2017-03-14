d1d1ecb49fdbf2e970ebaf0d30687f0a7a1022e9
Bretton Wade <bretton.wade@gmail.com>
2017-03-14 13:17:14 -0400

    update

--- src/program.js
+++ src/program.js
@@ -32,12 +32,22 @@ function test() {
         NEW("HTuple-3").point([0, 1, 0])
     ];
 
-    points[0].print("0");
+    points[0].print("0s");
     points[1].print("1");
     points[2].print("2");
 
     var plane = NEW("Hyperplane-3").initWithPoints(points);
 
     plane.print("plane");
+
+
+    var N = NEW("HTuple-3").point([1, 0, 0]);
+    var up = NEW("HTuple-3").point([0, 1, 0]);
+    var right = N.cross (up);
+    N.print("N");
+    up.print("up");
+    right.print("right");
+    up = right.cross (N);
+    up.print("up");
 
 }
